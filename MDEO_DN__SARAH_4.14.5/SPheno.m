1e1495c86d2aa9ca61c653ca391441cf21bbc004
david <david@debian>
2023-08-17 17:35:56 -0500

    I add model with Dirac Neutrinos

---
+++ MDEO_DN__SARAH_4.14.5/SPheno.m
@@ -0,0 +1,42 @@
+OnlyLowEnergySPheno = True;
+
+
+MINPAR={{1, Lambda1INPUT},
+        {2, Lambda2INPUT},
+        {3, Lambda3INPUT},
+        {10, g1pINPUT},
+        {11, g1p1INPUT},
+        {12, g11pINPUT},
+        {20, vXinput},
+        {21, ycinput}
+        };
+
+ParametersToSolveTadpoles = {MuP,mu2};
+
+DEFINITION[MatchingConditions]= {
+ {Ye, YeSM},
+ {Yd, YdSM},
+ {Yu, YuSM},
+ {g1, g1SM},
+ {g2, g2SM},
+ {g3, g3SM},
+ {vH, vSM}
+ };
+
+BoundaryLowScaleInput={
+ {g1p, g1pINPUT},
+ {g11p, g11pINPUT},
+ {g1p1, g1p1INPUT},
+ {L1, Lambda1INPUT},
+ {L2, Lambda2INPUT},
+ {L3, Lambda3INPUT},
+ {vX, vXinput},
+ {yc, ycinput},
+ {yx, LHInput[yx]}
+};
+
+AddTreeLevelUnitarityLimits=True;
+FlagLoopContributions = True;
+
+ListDecayParticles = {Fu,Fe,Fd, VZ, VWm, hh, VZp, Chi, Xi};
+ListDecayParticles3B = {{Fu,"Fu.f90"},{Fe,"Fe.f90"},{Fd,"Fd.f90"}};
