f57d29662f8277ee8fb624b013dc60961b777bf3
Andres Rivera <anferivera@gmail.com>
2024-02-13 17:37:44 -0500

    random_general_scan2.py

---
+++ random_general_scan2.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+
+import pyslha
+import numpy as np
+import pandas as pd
+import subprocess
+import time
+import sys
+
+import pyMDEO_LesHouches_generator
+import neutrino_analytic
+import NEUTRINO2018
+
+# Tiempo inicial
+t1=time.time() 
+
+x=[]
+
+#Num is the times to run the program. Entry using the keyboard
+Num = int(sys.argv[1])
+
+for i in range(0,Num):
+    
+    #Open the dicctionary
+    xdict = pyMDEO_LesHouches_generator.buildSLHAinFile()
+
+    #Random parameters
+    MZ = 91.1876
+    g1=3.55690247E-01
+    g1p = np.exp(np.random.uniform(np.log(10**(-3)),np.log(10**(0)))) #U1 coupling
+    epsilon = np.exp(np.random.uniform(np.log(10**(-6)),np.log(10**(-2))))
+    g1p1 = 0.
+    g11p = -g1*epsilon
+    #MZp = np.exp(np.random.uniform(np.log(9.6e1),np.log(5.0e2))) 
+    MZp = np.exp(np.random.uniform(np.log(1.0e0),np.log(9.1e1))) 
+    vX = MZp*(1.+epsilon**2)/(9.0*g1p)  #WARNING
+    VEV = 246.220569
+    #print('MZp=',MZp,g1p)
+    
+    theta = np.exp(np.random.uniform(np.log(1.0e-6),np.log(1.0e-3)))
+    gamma = 1.0/np.sqrt(1.0+np.tan(2.0*theta)**2.0)
+    
+    mh1=125.1
+    mh2= np.exp(np.random.uniform(np.log(mh1),np.log(5.0e3))) #WARNING
+    Lam1=(0.5/VEV**2.0)*(mh1**2.0+mh2**2.0-gamma*(mh2**2.0-mh1**2.0))
+    
+    Lam2 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) #conj[Et].Et.conj[Et].Et
+    Lam3 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) #conj[H].H.conj[Et].Et
+    Lam4 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) #conj[H].Et.conj[Et].H
+    Lam5 = (0.5/vX**2.0)*(mh1**2.0+mh2**2.0+gamma*(mh2**2.0-mh1**2.0)) ##conj[bi].bi.conj[bi].bi
+    Lam6 = (0.5/(VEV*vX))*gamma*(mh2**2.0-mh1**2.0)*np.tan(2.0*theta) ##conj[bi].bi.conj[H].H
+    Lam7 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) #conj[bi].bi.conj[Et].Et
+    Lam8 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) ##conj[S].S.conj[S].S
+    Lam9 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) #conj[S].S.conj[H].H
+    Lam10 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) #conj[S].S.conj[bi].bi
+    Lam11 = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) ##conj[S].S.conj[Et].Et
+    MS2 = np.exp(np.random.uniform(np.log(10**(6)),np.log(10**(8)))) #conj[S].S
+    Mn2 = np.exp(np.random.uniform(np.log(10**(6)),np.log(10**(8)))) #mEt2 conj[Et].Et
+    Yc = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(0)))) # Yc bi.CL.CR
+    muC = np.exp(np.random.uniform(np.log(10**(2)),np.log(2.*10**(3)))) #muC conj[H].Et.conj[S]
+
+    xdict.blocks['MINPAR'].entries[1]='%.6E    # lambda1Input'%Lam1
+    xdict.blocks['MINPAR'].entries[2]='%.6E    # lambda2Input'%Lam2
+    xdict.blocks['MINPAR'].entries[3]='%.6E    # lambda3Input'%Lam3
+    xdict.blocks['MINPAR'].entries[4]='%.6E    # lambda4Input'%Lam4
+    xdict.blocks['MINPAR'].entries[5]='%.6E    # lambda5Input'%Lam5
+    xdict.blocks['MINPAR'].entries[6]='%.6E    # lambda6Input'%Lam6
+    xdict.blocks['MINPAR'].entries[7]='%.6E    # lambda7Input'%Lam7
+    xdict.blocks['MINPAR'].entries[8]='%.6E    # lambda8Input'%Lam8
+    xdict.blocks['MINPAR'].entries[9]='%.6E    # lambda9Input'%Lam9
+    xdict.blocks['MINPAR'].entries[10]='%.6E    # lambda10Input'%Lam10
+    xdict.blocks['MINPAR'].entries[11]='%.6E    # lambda11Input'%Lam11
+    xdict.blocks['MINPAR'].entries[20]='%.6E    # g1pINPUT'%g1p
+    xdict.blocks['MINPAR'].entries[21]='%.6E    # g1p1INPUT'%g1p1
+    xdict.blocks['MINPAR'].entries[22]='%.6E    # g11pINPUT'%g11p
+    xdict.blocks['MINPAR'].entries[23]='%.6E    # mEt2Input'%Mn2
+    xdict.blocks['MINPAR'].entries[24]='%.6E    # MS2Input'%MS2
+    xdict.blocks['MINPAR'].entries[30]='%.6E    # vXinput'%vX
+    xdict.blocks['MINPAR'].entries[31]='%.6E    # Ycinput'%Yc
+    xdict.blocks['MINPAR'].entries[32]='%.6E    # muCinput'%muC
+    
+    mChi = Yc*vX/np.sqrt(2.)
+    
+    #if mChi < MZp:
+    #    continue
+    
+    MX1 = mChi+np.exp(np.random.uniform(np.log(1.0),np.log(1.0e3)))
+    MX2 = MX1+np.exp(np.random.uniform(np.log(1.0),np.log(1.0e3)))
+    thetaf = np.exp(np.random.uniform(np.log(1.0e-3),np.log(2.0*np.pi)))
+    gammaf = 1.0/np.sqrt(1.0+np.tan(2.0*thetaf)**2.0)
+    YX11 = np.sqrt(2.0)/vX*0.5*(MX1+MX2-gammaf*(MX2-MX1)) #Yx bi.FxL.FxR
+    YX22 = np.sqrt(2.0)/vX*0.5*(MX1+MX2+gammaf*(MX2-MX1))
+    YX12 = np.sqrt(2.0)/vX*0.5*gammaf*(MX2-MX1)*np.tan(2.0*thetaf)
+    YX21 = YX12
+    
+    xdict.blocks['YXIN'].entries[1,1]='%.6E    # YX(1,1)'%YX11
+    xdict.blocks['YXIN'].entries[1,2]='%.6E    # YX(1,2)'%YX12
+    xdict.blocks['YXIN'].entries[2,1]='%.6E    # YX(2,1)'%YX21
+    xdict.blocks['YXIN'].entries[2,2]='%.6E    # YX(2,2)'%YX22
+
+    #Rotation matrices and Yukawas after inversion
+    FMa = neutrino_analytic.MATFermionXi(YX11,YX12,YX21,YX22,vX)
+    SMa = neutrino_analytic.MATSalarS0(Lam3,Lam4,Lam7,Lam9,Lam10,vX,VEV,muC,MS2,Mn2)
+    YUKS = neutrino_analytic.Yukawas(NEUTRINO2018.nuexpvalues(),FMa,SMa)
+
+    YnL11 = YUKS['YnL11']
+    YnL12 = YUKS['YnL12']
+    YnL13 = YUKS['YnL13']
+    YnL21 = YUKS['YnL21']
+    YnL22 = YUKS['YnL22']
+    YnL23 = YUKS['YnL23']
+    YnR11 = YUKS['YnR11']
+    YnR12 = YUKS['YnR12']
+    YnR13 = YUKS['YnR13']
+    YnR21 = YUKS['YnR21']
+    YnR22 = YUKS['YnR22']
+    YnR23 = YUKS['YnR23']
+
+    ZL11 = YUKS['ZL11']
+    ZL12 = YUKS['ZL12']
+    ZL21 = YUKS['ZL21']
+    ZL22 = YUKS['ZL22']
+    ZR11 = YUKS['ZR11']
+    ZR12 = YUKS['ZR12']
+    ZR21 = YUKS['ZR21']
+    ZR22 = YUKS['ZR22']
+    mXi_1 = YUKS['mXi_1']
+    mXi_2 = YUKS['mXi_2']
+
+    ZN11 = YUKS['ZN11']
+    ZN12 = YUKS['ZN12']
+    ZN21 = YUKS['ZN21']
+    ZN22 = YUKS['ZN22']
+    mns_1 = YUKS['mns_1']
+    mns_2 = YUKS['mns_2']
+    
+    #No scalar DM
+    if mns_1 < mChi or mns_1 < mXi_1:
+        continue
+
+    ##hdij->YnLij
+    xdict.blocks['YNLIN'].entries[1,1]='%.6E    # YnL(1,1)'%YnL11
+    xdict.blocks['YNLIN'].entries[1,2]='%.6E    # YnL(1,2)'%YnL12
+    xdict.blocks['YNLIN'].entries[1,3]='%.6E    # YnL(1,3)'%YnL13
+    xdict.blocks['YNLIN'].entries[2,1]='%.6E    # YnL(2,1)'%YnL21
+    xdict.blocks['YNLIN'].entries[2,2]='%.6E    # YnL(2,2)'%YnL22
+    xdict.blocks['YNLIN'].entries[2,3]='%.6E    # YnL(2,3)'%YnL23
+    ##heij->YnRij
+    xdict.blocks['YNRIN'].entries[1,1]='%.6E    # YnR(1,1)'%YnR11
+    xdict.blocks['YNRIN'].entries[1,2]='%.6E    # YnR(1,2)'%YnR12
+    xdict.blocks['YNRIN'].entries[1,3]='%.6E    # YnR(1,3)'%YnR13
+    xdict.blocks['YNRIN'].entries[2,1]='%.6E    # YnR(2,1)'%YnR21
+    xdict.blocks['YNRIN'].entries[2,2]='%.6E    # YnR(2,2)'%YnR22
+    xdict.blocks['YNRIN'].entries[2,3]='%.6E    # YnR(2,3)'%YnR23
+
+    #Write the Leshouches file
+    if MZp < MZ:
+        pyslha.writeSLHAFile('LesHouches.in.MDEOlight_low',xdict)
+    else:
+        pyslha.writeSLHAFile('LesHouches.in.MDEO_low',xdict)
+    
+    if MZp < MZ:
+        
+        #run SPheno
+        spheno = subprocess.getoutput('../.././SPheno-4.0.5/bin/SPhenoMDEOlight LesHouches.in.MDEOlight_low')
+        so = subprocess.getoutput('cat SPheno.spc.MDEOlight')
+        #run micromegas.
+        mo = subprocess.getoutput('~/Work/micromegas_5.0.6/MDEOlight/./CalcOmega_with_DI_Detection')
+    
+    else:
+        #run SPheno
+        spheno = subprocess.getoutput('../.././SPheno-4.0.5/bin/SPhenoMDEO LesHouches.in.MDEO_low')
+        so = subprocess.getoutput('cat SPheno.spc.MDEO')
+        #run micromegas.
+        mo = subprocess.getoutput('~/Work/micromegas_5.0.6/MDEO/./CalcOmega_with_DI_Detection')
+    
+    T = eval(so.split('Block SPhenoLowEnergy #')[1].split()[4])
+    S = eval(so.split('Block SPhenoLowEnergy #')[1].split()[10])
+    U = eval(so.split('Block SPhenoLowEnergy #')[1].split()[16])
+    g2mu = eval(so.split('Block SPhenoLowEnergy #')[1].split()[26])
+    TWpSPheno = eval(so.split('Block ANGLES Q')[1].split()[6])
+    MZpSPheno = eval(so.split('# VWm')[1].split()[1])
+
+    if len(mo.split()) == 2:
+        continue     
+    
+    Omega1 = eval(mo.split('Omega_1h^2=')[1].split()[0])
+    Omega2 = eval(mo.split('Omega_2h^2=')[1].split()[0])
+    #print("i=",i,"Omega1=",Omega1,"Omega2=",Omega2)
+    
+    if Omega1+Omega2 > 0.1236:
+        continue
+    
+    SIN1= eval(mo.split('CDM1-nucleon cross sections[pb]:')[1].split()[7])
+    SIN2= eval(mo.split('CDM2-nucleon cross sections[pb]:')[1].split()[7])
+    sv = eval(mo.split('CDM2-nucleon cross sections[pb]:')[1].split()[7])
+    
+    x.append([Lam1,Lam2,Lam3,Lam4,Lam5,Lam6,Lam7,Lam8,Lam9,Lam10,Lam11,MS2,Mn2,vX,Yc,muC,g1p,epsilon,\
+              ZL11,ZL12,ZL21,ZL22,ZR11,ZR12,ZR21,ZR22,\
+          ZN11,ZN12,ZN21,ZN22,mXi_1,mXi_2,mns_1,mns_2,YnL11,YnL12,YnL13,YnL21,YnL22,YnL23,\
+          YnR11,YnR12,YnR13,YnR21,YnR22,YnR23,Omega1,Omega2,SIN1,SIN2,sv,MZp,mh1,mh2,theta,thetaf,T,S,U,\
+              g2mu,mChi,TWpSPheno,MZpSPheno])
+
+x=np.asarray(x)
+
+xd=pd.DataFrame(x,columns=['Lam1','Lam2','Lam3','Lam4','Lam5','Lam6','Lam7','Lam8','Lam9',\
+                           'Lam10','Lam11','MS2','Mn2','vX','Yc','muC','g1p','epsilon',\
+                           'ZL11','ZL12','ZL21','ZL22','ZR11','ZR12','ZR21','ZR22','ZN11',\
+                           'ZN12','ZN21','ZN22','mXi_1','mXi_2','mns_1','mns_2','YnL11',\
+                           'YnL12','YnL13','YnL21','YnL22','YnL23','YnR11','YnR12','YnR13',\
+                           'YnR21','YnR22','YnR23','Omega1','Omega2','SIN1','SIN2','sv',\
+                           'MZp','mh1','mh2','theta','thetaf','T','S','U','g2mu','mChi',\
+                           'TWpSPheno','MZpSPheno'])
+
+#argv[2] will be the number in the end of the file extension: example: xd_scan1.csv
+xd.to_csv('xd_scan-general-'+sys.argv[2]+'.csv')
+
+t2=time.time() 
+print ("The program spent", t2-t1, "s running",Num,"times -> xd_scan",sys.argv[2],".csv")
